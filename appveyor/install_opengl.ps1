5b680b588b238ef8aef9cf8748a08b9de17a7f25
David Hoese <david.hoese@ssec.wisc.edu>
2020-03-03 14:24:41 -0600

    Add raw=true to dll download

--- appveyor/install_opengl.ps1
+++ appveyor/install_opengl.ps1
@@ -15,7 +15,7 @@ function DownloadMesaOpenGL ($architecture) {
     [Net.ServicePointManager]::SecurityProtocol = 'Ssl3, Tls, Tls11, Tls12'
     $webclient = New-Object System.Net.WebClient
     # Download and retry up to 3 times in case of network transient errors.
-    $url = $MESA_GL_URL + "opengl32_mingw_" + $architecture + ".dll"
+    $url = $MESA_GL_URL + "opengl32_mingw_" + $architecture + ".dll?raw=true"
     if ($architecture -eq "32") {
         $filepath = "C:\Windows\SysWOW64\opengl32.dll"
     } else {
